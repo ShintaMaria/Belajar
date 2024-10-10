0bcf0bf2cb2212a8e78939227d9785dd1c5096a0
ShintaMaria <158575036+ShintaMaria@users.noreply.github.com>
2024-10-10 11:41:10 +0700

    Create ScrollView.java

---
+++ app/src/main/java/com/example/belajar/ScrollView.java
@@ -0,0 +1,184 @@
+package com.example.belajar;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ScrollView extends AppCompatActivity {
+
+    private TextView loginScreenText, welcomeText;
+    private Button buttonTwo, buttonThree, buttonFour, buttonFive, buttonSix, buttonSeven, buttonEight, buttonNine, buttonTen, button11,button12,button13, button14,button15,button16,button17,button18,button19,button20,button21;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scroll_view);
+
+        // Menghubungkan tampilan XML dengan MainActivity
+        loginScreenText = findViewById(R.id.loginscrn);
+        welcomeText = findViewById(R.id.fstTxt);
+        buttonTwo = findViewById(R.id.buttonTwo);
+        buttonThree = findViewById(R.id.buttonThree);
+        buttonFour = findViewById(R.id.buttonFour);
+        buttonFive = findViewById(R.id.buttonFive);
+        buttonSix = findViewById(R.id.buttonSix);
+        buttonSeven = findViewById(R.id.buttonSeven);
+        buttonEight = findViewById(R.id.buttonEight);
+        buttonNine = findViewById(R.id.buttonNine);
+        buttonTen = findViewById(R.id.buttonTen);
+        button11 = findViewById(R.id.button11);
+        button12 = findViewById(R.id.button12);
+        button13 = findViewById(R.id.button13);
+        button14 = findViewById(R.id.button14);
+        button15 = findViewById(R.id.button15);
+        button16 = findViewById(R.id.button16);
+        button17 = findViewById(R.id.button17);
+        button18 = findViewById(R.id.button18);
+        // Menambahkan aksi klik untuk masing-masing tombol
+        buttonTwo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Pindah ke SecondActivity
+                Intent intent = new Intent(ScrollView.this, LinearLayout9.class);
+                startActivity(intent);
+            }
+        });
+
+        buttonThree.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ScrollView.this, RelativeLayout.class);
+                startActivity(intent);
+            }
+        });
+
+        buttonFour.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ScrollView.this, ConstraintLayout.class);
+                startActivity(intent);
+            }
+        });
+
+        buttonFive.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ScrollView.this, FrameLayout.class);
+                startActivity(intent);
+            }
+        });
+
+        buttonSix.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ScrollView.this, TabelLayout.class);
+                startActivity(intent);
+            }
+        });
+
+        buttonSeven.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ScrollView.this, MaterialDesign.class);
+                startActivity(intent);
+            }
+        });
+
+        buttonEight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ScrollView.this, HorizontalScrollVieww.class);
+                startActivity(intent);
+            }
+        });
+
+        buttonNine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ScrollView.this, Stringg.class);
+                startActivity(intent);
+            }
+        });
+
+        buttonTen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ScrollView.this, AutoCompleteTextView.class);
+                startActivity(intent);
+            }
+        });
+
+        button11.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ScrollView.this, Fragmentt.class);
+                startActivity(intent);
+            }
+        });
+
+        button12.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ScrollView.this, IntentImplicit.class);
+                startActivity(intent);
+            }
+        });
+
+        button13.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ScrollView.this, IntentExplicit.class);
+                startActivity(intent);
+            }
+        });
+
+        button14.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ScrollView.this, ManagementFile1.class);
+                startActivity(intent);
+            }
+        });
+
+        button15.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ScrollView.this, ImplementasiSQLite.class);
+                startActivity(intent);
+            }
+        });
+
+        button16.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ScrollView.this, KonsepPreferences30.class);
+                startActivity(intent);
+            }
+        });
+
+        button17.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ScrollView.this, RecycleView.class);
+                startActivity(intent);
+            }
+        });
+
+        button18.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ScrollView.this, LBS33.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+    }
+}
