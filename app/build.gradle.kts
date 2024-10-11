41566f7727f8f189f5391715b70b7667120387e0
ShintaMaria <158575036+ShintaMaria@users.noreply.github.com>
2024-10-11 10:01:06 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,3 @@
-//val implementation: Unit
-//    get() {
-//        TODO()
-//    }
-
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.google.android.libraries.mapsplatform.secrets.gradle.plugin)
@@ -42,6 +37,8 @@ android {
 
 dependencies {
 
+    implementation("com.jjoe64:graphview:4.2.2")
+
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
@@ -51,25 +48,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-//    implementation 'androidx.recyclerview:recyclerview:1.2.1'
-//    implementation 'androidx.cardview:cardview:1.0.0'
 
-//
-//    // AndroidX RecyclerView library
-//    implementation 'androidx.recyclerview:recyclerview:1.2.1'
-//
-//    // AndroidX CardView library
-//    implementation 'androidx.cardview:cardview:1.0.0'
-//
-//    // AndroidX AppCompat library
-//    implementation 'androidx.appcompat:appcompat:1.3.1'
-//
-//    // Optional: KTX library for easier Kotlin usage
-//    implementation 'androidx.core:core-ktx:1.6.0'
-
-//    // Testing dependencies
-//    testImplementation 'junit:junit:4.13.2'
-//    androidTestImplementation 'androidx.test.ext:junit:1.1.3'
-//    androidTestImplementation 'androidx.test.espresso:espresso-core:3.4.0'
 
 }
