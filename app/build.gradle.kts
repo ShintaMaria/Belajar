01ce55f3273690703e6e009f02b49bbbf0194fd2
ShintaMaria <158575036+ShintaMaria@users.noreply.github.com>
2024-10-12 11:07:03 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,8 +37,6 @@ android {
 
 dependencies {
 
-    implementation("com.jjoe64:graphview:4.2.2")
-
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
