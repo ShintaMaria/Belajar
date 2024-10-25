f5e5230cd2809014b340acaee4b2554e5796b62d
ShintaMaria <158575036+ShintaMaria@users.noreply.github.com>
2024-10-25 18:15:52 +0700

    Create MovieAdapter37.java

---
+++ app/src/main/java/com/example/belajar/MovieAdapter37.java
@@ -0,0 +1,101 @@
+package com.example.belajar;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
+import com.bumptech.glide.request.RequestOptions;
+
+import java.util.List;
+
+public class MovieAdapter37 extends RecyclerView.Adapter<MovieAdapter37.ViewHolder> {
+
+    private List<ModelMovie37> items;
+    private MovieAdapter37.OnSelectData onSelectData;
+    private Context context;
+    private double Rating;
+
+    public interface OnSelectData {
+        void onSelected(ModelMovie37 modelMovie);
+    }
+
+    public MovieAdapter37(Context context, List<ModelMovie37> items,
+                        MovieAdapter37.OnSelectData onSelectData) {
+        this.context = context;
+        this.items = items;
+        this.onSelectData = onSelectData;
+    }
+
+    @Override
+    public MovieAdapter37.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_film37, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        final ModelMovie37 data = items.get(position);
+        Rating = data.getVoteAverage();
+        holder.tvTitle.setText(data.getTitle());
+        holder.tvReleaseDate.setText(data.getReleaseDate());
+        holder.tvDesc.setText(data.getOverview());
+
+        // Set rating bar
+        float newValue = (float) Rating;
+        holder.ratingBar.setNumStars(5);
+        holder.ratingBar.setStepSize(0.5f);
+        holder.ratingBar.setRating(newValue / 2);
+
+        // Load image using Glide
+        Glide.with(context)
+                .load(ApiEndPoint37.URLIMAGE)
+                .apply(new RequestOptions()
+                        .placeholder(R.drawable.ic_image)
+                        .transform( new RoundedCorners(16)))
+                .into(holder.imgPhoto);
+
+        // Set click listener
+        holder.cvFilm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onSelectData.onSelected(data);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        Log.d("MovieAdapter", "Jumlah item: " + items.size());
+        return items.size();
+    }
+
+    // ViewHolder class
+    class ViewHolder extends RecyclerView.ViewHolder {
+        public CardView cvFilm;
+        public ImageView imgPhoto;
+        public TextView tvTitle;
+        public TextView tvReleaseDate;
+        public TextView tvDesc;
+        public RatingBar ratingBar;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            cvFilm = itemView.findViewById(R.id.cvFilm);
+            imgPhoto = itemView.findViewById(R.id.imgPhoto);
+            tvTitle = itemView.findViewById(R.id.tvTitle);
+            tvReleaseDate = itemView.findViewById(R.id.tvReleaseDate);
+            tvDesc = itemView.findViewById(R.id.tvDesc);
+            ratingBar = itemView.findViewById(R.id.ratingBar);
+        }
+    }
+}
