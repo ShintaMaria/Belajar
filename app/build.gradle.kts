37445c2d725a86b22605512d5cb16ba23c83dbd2
ShintaMaria <158575036+ShintaMaria@users.noreply.github.com>
2024-10-25 18:15:02 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,7 +36,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
@@ -48,8 +47,18 @@ dependencies {
     androidTestImplementation(libs.espresso.core)
     implementation("com.google.android.gms:play-services-maps:18.0.1")
     implementation("com.google.android.gms:play-services-location:21.0.1")
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.ext.junit)
-    androidTestImplementation(libs.espresso.core)
+
+    // JSON
+    dependencies {
+        implementation("com.github.amitshekhariitbhu.Fast-Android-Networking:android-networking:1.0.4")
+        implementation("com.ramotion.cardslider:card-slider:0.3.1")
+        implementation("com.github.bumptech.glide:glide:4.14.2")
+        implementation("com.github.florent37:diagonallayout:1.0.7")
+        implementation("com.github.ivbaranov:materialfavoritebutton:0.1.5")
+        annotationProcessor("com.github.bumptech.glide:compiler:4.14.2")
+        implementation("com.flaviofaria:kenburnsview:1.0.7")
+        implementation ("androidx.recyclerview:recyclerview:1.2.1")
+        implementation(libs.transport.api)
+    }
 
 }
