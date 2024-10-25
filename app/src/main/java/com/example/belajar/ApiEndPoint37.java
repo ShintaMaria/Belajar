48d61c6dd7e665ae37ef0bde36fd7e400d90339d
ShintaMaria <158575036+ShintaMaria@users.noreply.github.com>
2024-10-25 18:16:29 +0700

    Create ApiEndPoint37.java

---
+++ app/src/main/java/com/example/belajar/ApiEndPoint37.java
@@ -0,0 +1,13 @@
+package com.example.belajar;
+
+public class ApiEndPoint37 {
+    public static String BASEURL = "https://api.themoviedb.org/3/";
+    public static String APIKEY = "api_key=ddb335a75ea4eb8af10b36fce9df8dc0";
+    public static String LANGUAGE = "&language=en-US";
+    public static String SEARCH_MOVIE = "search/movie?";
+    public static String QUERY = "&query=";
+    public static String MOVIE_PLAYNOW = "movie/now_playing?";
+    public static String MOVIE_POPULAR = "discover/movie?";
+    public static String URLIMAGE = "https://image.tmdb.org/t/p/w500/1E5baAaEse26fej7uHcjOgEE2t2.jpg";
+}
+
