d80d1e9b8c780ffb1882baf721e13110985e8a11
ShintaMaria <158575036+ShintaMaria@users.noreply.github.com>
2024-10-31 19:39:25 +0700

    Update ApiService.java

--- app/src/main/java/com/example/belajar/ApiService.java
+++ app/src/main/java/com/example/belajar/ApiService.java
@@ -1,4 +1,6 @@
 package com.example.belajar;
+
+// import pustaka retrofit untuk membuat panggilan api
 import retrofit2.Call;
 import com.example.belajar.LoginResponse;
 
@@ -9,15 +11,23 @@ import retrofit2.http.GET;
 import retrofit2.http.POST;
 import retrofit2.http.Query;
 
+// antarmuka untuk mendefinisikan endpoint api
 public interface ApiService {
+
+    // endpoint untuk melakukan login
     @FormUrlEncoded
     @POST("login.php")
     Call<LoginResponse> login(
             @Field("username") String username,
             @Field("password") String password
     );
-    @GET("getData.php")
-    Call<UserResponse> getData(@Query("token") String token);
 
+    // endpoint untuk mendapatkan data user dengan token
+    @GET("getData.php")
+    Call<UserResponse> getData(@Query("token") String token);  // mengirimkan token sebagai query parameter
 
 }
+
+//ApiService adalah interface Retrofit yang mendefinisikan endpoint API, seperti:
+//login: Metode POST untuk memverifikasi username dan password.
+//getData: Metode GET yang mengambil data pengguna berdasarkan token yang diterima setelah login berhasil.
