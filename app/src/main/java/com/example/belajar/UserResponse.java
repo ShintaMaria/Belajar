60578871169e8e95cb2d40dafc53743310b75bdb
ShintaMaria <158575036+ShintaMaria@users.noreply.github.com>
2024-10-31 19:39:03 +0700

    Update UserResponse.java

--- app/src/main/java/com/example/belajar/UserResponse.java
+++ app/src/main/java/com/example/belajar/UserResponse.java
@@ -1,23 +1,30 @@
 package com.example.belajar;
 
+// kelas UserResponse untuk menyimpan respons API yang mencakup status dan data pengguna
 public class UserResponse {
+    // variabel untuk menyimpan status respons dan objek data pengguna
     private String status;
     private User data;
 
-    // Getter dan Setter untuk status dan data
+    // getter untuk mengambil nilai status respons
     public String getStatus() {
         return status;
     }
 
+    // setter untuk menetapkan nilai status respons
     public void setStatus(String status) {
         this.status = status;
     }
 
+    // getter untuk mengambil data pengguna dalam bentuk objek User
     public User getData() {
         return data;
     }
 
+    // setter untuk menetapkan data pengguna
     public void setData(User data) {
         this.data = data;
     }
 }
+
+// menampilkan status dan data pengguna yang berhasil diambil.
