b9e8997cdb1407d4100aad2d7246e3d89dca6ceb
ShintaMaria <158575036+ShintaMaria@users.noreply.github.com>
2024-10-31 07:02:13 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,5 +60,11 @@ dependencies {
         implementation ("androidx.recyclerview:recyclerview:1.2.1")
         implementation(libs.transport.api)
     }
+    implementation("com.github.ivbaranov:materialfavoritebutton:0.1.5")
+    implementation("com.github.bumptech.glide:glide:4.11.0")
+    implementation("com.github.bumptech.glide:compiler:4.11.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.android.volley:volley:1.2.1")
 
 }
