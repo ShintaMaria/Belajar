39f2898a48c7b4323f346238cbd832f39bfd8935
ShintaMaria <158575036+ShintaMaria@users.noreply.github.com>
2024-10-31 19:39:17 +0700

    Update LoginResponse.java

--- app/src/main/java/com/example/belajar/LoginResponse.java
+++ app/src/main/java/com/example/belajar/LoginResponse.java
@@ -1,24 +1,30 @@
 package com.example.belajar;
 
+// kelas LoginResponse untuk menyimpan respons dari API saat login
 public class LoginResponse {
+    // variabel untuk status login dan token autentikasi
     private String status;
     private String token;
 
-    // Getter dan Setter untuk status dan token
+    // getter untuk mengambil nilai status
     public String getStatus() {
         return status;
     }
 
+    // setter untuk menetapkan nilai status
     public void setStatus(String status) {
         this.status = status;
     }
 
+    // getter untuk mengambil nilai token
     public String getToken() {
         return token;
     }
 
+    // setter untuk menetapkan nilai token
     public void setToken(String token) {
         this.token = token;
     }
 }
 
+// menyimpan status dan token dari server yang digunakan sebagai tanda berhasil atau tidaknya login.
