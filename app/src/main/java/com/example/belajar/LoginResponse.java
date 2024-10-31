2d139b8e5dc7dec8ea65c4ce68ff34d1b39559c6
ShintaMaria <158575036+ShintaMaria@users.noreply.github.com>
2024-10-31 07:01:55 +0700

    Create LoginResponse.java

---
+++ app/src/main/java/com/example/belajar/LoginResponse.java
@@ -0,0 +1,24 @@
+package com.example.belajar;
+
+public class LoginResponse {
+    private String status;
+    private String token;
+
+    // Getter dan Setter untuk status dan token
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
+
